ff69ac787c3801a90e57f77e87d6e51708fb37cd
Carlos Saito <carlos@saito.style>
2019-12-25 12:35:50 +0100

    Replace board vec to array

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
+const WIDTH: usize = 7;
 
 fn main() {
     println!("Welcome to four-in-a-row!!! (implemented in Rust)");
-    let width = 7;
-    let mut board: Vec<i32> = Vec::new();
-
-    for _v in 0..width {
-        board.push(0);
-    }
+    let mut board: [i32; WIDTH] = [0; WIDTH];
 
     print_board(&board);
     play_piece(&mut board, 1);
@@ -15,11 +11,11 @@ fn main() {
     print_board(&board);
 }
 
-fn print_board(board: &Vec<i32>) {
+fn print_board(board: &[i32; WIDTH]) {
     let height = 6;
 
     for h in 0..height {
-        for w in board {
+        for w in board.iter() {
             if *w >= height - h {
                 print!("X ");
             } else {
@@ -36,6 +32,6 @@ fn print_board(board: &Vec<i32>) {
     println!()
 }
 
-fn play_piece(board: &mut Vec<i32>, index: usize) {
+fn play_piece(board: &mut [i32; WIDTH], index: usize) {
     board[index] = board[index] + 1;
 }
