3a140f7f6e7f53cdbf594ca3f62533e7cc014ccb
Carlos Saito <carlos@saito.style>
2019-12-25 13:04:16 +0100

    Implement board as type

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,52 @@
+use std::fmt;
+
 const WIDTH: usize = 7;
+
+struct Board {
+    columns: [i32; WIDTH],
+}
+
+impl Board {
+    fn new() -> Board {
+        Board {
+            columns: [0; WIDTH]
+        }
+    }
+
+    fn play(&mut self, index: usize) {
+        self.columns[index] = self.columns[index] + 1;
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let height: i32 = 6;
+
+        for h in 0..height {
+            for w in self.columns.iter() {
+                if *w >= height - h {
+                    write!(f, "X ")?
+                } else {
+                    write!(f, "· ")?
+                }
+            }
+            writeln!(f)?
+        }
+
+        for n in 0..WIDTH {
+            write!(f, "{} ", n)?
+        }
+
+
+        writeln!(f, "")
+    }
+}
 
 fn main() {
     println!("Welcome to four-in-a-row!!! (implemented in Rust)");
-    let mut board: [i32; WIDTH] = [0; WIDTH];
-
-    print_board(&board);
-    play_piece(&mut board, 1);
-    play_piece(&mut board, 1);
-    play_piece(&mut board, 2);
-    print_board(&board);
-}
-
-fn print_board(board: &[i32; WIDTH]) {
-    let height = 6;
-
-    for h in 0..height {
-        for w in board.iter() {
-            if *w >= height - h {
-                print!("X ");
-            } else {
-                print!("· ");
-            }
-        }
-        println!();
-    }
-
-    for n in 0..board.len() {
-        print!("{} ", n);
-    }
-
-    println!()
-}
-
-fn play_piece(board: &mut [i32; WIDTH], index: usize) {
-    board[index] = board[index] + 1;
+    let mut my_board = Board::new();
+    my_board.play(1);
+    my_board.play(1);
+    my_board.play(2);
+    println!("{}", my_board);
 }
