02c1d8782f3cb5e8c9772e1395e3cc402c925292
Carlos Saito <carlos@saito.style>
2019-12-25 13:11:28 +0100

    Split "Board" to its file

---
+++ src/board.rs
@@ -0,0 +1,41 @@
+const WIDTH: usize = 7;
+const HEIGHT: i32 = 6;
+
+use std::fmt;
+
+pub struct Board {
+    columns: [i32; WIDTH],
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Board {
+            columns: [0; WIDTH]
+        }
+    }
+
+    pub fn play(&mut self, index: usize) {
+        self.columns[index] = self.columns[index] + 1;
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for h in 0..HEIGHT {
+            for w in self.columns.iter() {
+                if *w >= HEIGHT - h {
+                    write!(f, "X ")?
+                } else {
+                    write!(f, "· ")?
+                }
+            }
+            writeln!(f)?
+        }
+
+        for n in 0..WIDTH {
+            write!(f, "{} ", n)?
+        }
+
+        writeln!(f, "")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,8 @@
-use std::fmt;
-
-const WIDTH: usize = 7;
-
-struct Board {
-    columns: [i32; WIDTH],
-}
-
-impl Board {
-    fn new() -> Board {
-        Board {
-            columns: [0; WIDTH]
-        }
-    }
-
-    fn play(&mut self, index: usize) {
-        self.columns[index] = self.columns[index] + 1;
-    }
-}
-
-impl fmt::Display for Board {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let height: i32 = 6;
-
-        for h in 0..height {
-            for w in self.columns.iter() {
-                if *w >= height - h {
-                    write!(f, "X ")?
-                } else {
-                    write!(f, "· ")?
-                }
-            }
-            writeln!(f)?
-        }
-
-        for n in 0..WIDTH {
-            write!(f, "{} ", n)?
-        }
-
-
-        writeln!(f, "")
-    }
-}
+mod board;
 
 fn main() {
+    use board::Board;
+
     println!("Welcome to four-in-a-row!!! (implemented in Rust)");
     let mut my_board = Board::new();
     my_board.play(1);
