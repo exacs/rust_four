862586fd89b802ba295723dafcc99a4250db94a8
Carlos Saito <carlos@saito.style>
2019-12-26 22:19:35 +0100

    Implement a human vs human

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use game::Game;
 fn main() {
     let mut my_game = Game::new(7, 6);
     println!("Welcome to four-in-a-row!!! (implemented in Rust)");
-    let a = player::human_player(&my_game);
-    my_game.play(a);
+    while my_game.turn() != None {
+        let a = player::human_player(&my_game);
+        my_game.play(a);
+    }
     println!("{}", my_game);
 }
