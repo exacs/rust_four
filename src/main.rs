6767c890366826c88e2ae094278125b67ff48ad5
Carlos Saito <carlos@saito.style>
2019-12-26 22:52:04 +0100

    Refactor human_player to its struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod game;
 mod player;
 use game::Game;
+use player::HumanPlayer;
 
 fn main() {
     let mut my_game = Game::new(7, 6);
     println!("Welcome to four-in-a-row!!! (implemented in Rust)");
     while my_game.turn() != None {
-        let a = player::human_player(&my_game);
+        let a = HumanPlayer::next_movement(&my_game);
         my_game.play(a);
     }
     println!("{}", my_game);
--- src/player.rs
+++ src/player.rs
@@ -1,11 +1,15 @@
 use crate::game::Game;
 use std::io;
 
-pub fn human_player (game: &Game) -> i32 {
-  println!("{}", game);
-  let mut input = String::new();
-  io::stdin().read_line(&mut input)
-    .expect("Failed to read line");
+pub struct HumanPlayer;
 
-  return input.trim().parse::<i32>().expect("Not parseable!!");
+impl HumanPlayer {
+  pub fn next_movement(game: &Game) -> i32 {
+    println!("{}", game);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+      .expect("Failed to read line");
+
+    return input.trim().parse::<i32>().expect("Not parseable!!");
+  }
 }
