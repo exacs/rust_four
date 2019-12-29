7f31cacc4cfa8ca8fcc739822bb28389cd6c3f2f
Carlos Saito <carlos@saito.style>
2019-12-29 20:19:45 +0100

    Split BoardIterator to its own file

---
+++ src/board/iterator.rs
@@ -0,0 +1,53 @@
+use super::*;
+
+pub struct BoardIterator {
+    width: i32,
+    height: i32,
+    i: i32,
+    j: i32,
+    k: usize,
+}
+
+impl BoardIterator {
+    pub fn new(width: i32, height: i32) -> BoardIterator {
+        BoardIterator {
+            width,
+            height,
+            i: -1,
+            j: 0,
+            k: 0,
+        }
+    }
+}
+
+impl Iterator for BoardIterator {
+    type Item = ((i32, i32), Direction);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let directions = [
+            Direction::South,
+            Direction::East,
+            Direction::SouthEast,
+            Direction::SouthWest,
+        ];
+
+        self.i += 1;
+
+        if self.i >= self.width {
+            self.j += 1;
+            self.i = 0;
+        }
+
+        if self.j >= self.height {
+            self.k += 1;
+            self.j = 0;
+            self.i = 0;
+        }
+
+        if self.k >= directions.len() {
+            return None;
+        }
+
+        return Some(((self.i, self.j), directions[self.k]));
+    }
+}
--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,3 +1,6 @@
+mod iterator;
+
+use iterator::BoardIterator;
 use std::collections::HashMap;
 use std::fmt;
 
@@ -28,14 +31,6 @@ pub enum Direction {
     East,
     SouthEast,
     SouthWest,
-}
-
-pub struct BoardIterator {
-    width: i32,
-    height: i32,
-    i: i32,
-    j: i32,
-    k: usize,
 }
 
 impl Board {
@@ -105,13 +100,7 @@ impl Board {
     }
 
     pub fn all_iter(&self) -> BoardIterator {
-        BoardIterator {
-            width: self.width,
-            height: self.height,
-            i: -1,
-            j: 0,
-            k: 0,
-        }
+        BoardIterator::new(self.width, self.height)
     }
 }
 
@@ -133,37 +122,5 @@ impl fmt::Display for Board {
         }
 
         writeln!(f, "")
-    }
-}
-
-impl Iterator for BoardIterator {
-    type Item = ((i32, i32), Direction);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let directions = [
-            Direction::South,
-            Direction::East,
-            Direction::SouthEast,
-            Direction::SouthWest,
-        ];
-
-        self.i += 1;
-
-        if self.i >= self.width {
-            self.j += 1;
-            self.i = 0;
-        }
-
-        if self.j >= self.height {
-            self.k += 1;
-            self.j = 0;
-            self.i = 0;
-        }
-
-        if self.k >= directions.len() {
-            return None;
-        }
-
-        return Some(((self.i, self.j), directions[self.k]));
     }
 }
